e6874703a9e42f6a8ab23c0119a7c1caf65e87bc
Krishna <multiqostech@gmail.com>
2022-04-18 11:58:19 +0530

    Initial Commit

---
+++ AWS-Face-Rekognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWS-Face-Rekognition
+//
+//  Created by MQF-1 on 18/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
